939b6f34f7c7b6ff8aa2202a1e12749332dea56d
Stefan Monov <logixoul@gmail.com>
2026-02-17 09:30:56 +0200

    closer to building/launching on wsl

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
-#include "raylib.h"
+#include "ui.h"
 
-int main(void)
+int main()
 {
-    InitWindow(800, 450, "init Lab audio demo");
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
-
-    CloseWindow();
-
-    return 0;
+    AudioDemo::Ui ui;
+    ui.mainLoop();
 }
--- src/ui.h
+++ src/ui.h
@@ -1,1 +1,27 @@
 #pragma once
+#include <raylib.h>
+
+namespace AudioDemo {
+
+class Ui {
+public:
+    Ui() {
+        InitWindow(800, 450, "init Lab audio demo");
+    }
+
+    ~Ui() {
+        CloseWindow();
+    }
+
+    void mainLoop() {
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            EndDrawing();
+        }
+    }
+};
+
+}
